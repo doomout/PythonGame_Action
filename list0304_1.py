eec69014f0ad149de89d39dff2973c171e5445f1
khg <winquake3@hanmail.net>
2023-02-04 20:05:55 +0900

    캐릭터 에니메이션 구현

--- list0304_1.py
+++ list0304_1.py
@@ -17,9 +17,14 @@ DIR_UP = 0
 DIR_DOWN = 1
 DIR_LEFT = 2
 DIR_RIGHT = 3
+ANIMATION = [0,1,0,2] #에니메이션 번호 정의
+
+tmr = 0 #타이머
 
 pen_x = 90
 pen_y = 90
+pen_d = 0 #펜펜의 방향
+pen_a = 0 #펜펜의 이미지 번호
 
 map_data = [
     [0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0],
@@ -39,7 +44,7 @@ def draw_screen():  # 게임 화면 그리기
     for y in range(9): #미로 그리기
         for x in range(12):
             canvas.create_image(x * 60 + 30, y * 60 + 30, image=img_bg[map_data[y][x]], tag="SCREEN")
-    canvas.create_image(pen_x, pen_y, image=img_pen, tag="SCREEN")
+    canvas.create_image(pen_x, pen_y, image=img_pen[pen_a], tag="SCREEN")
 
 
 def check_wall(cx, cy, di):  # 각 방향에 벽 존재 여부 확인
@@ -68,23 +73,29 @@ def check_wall(cx, cy, di):  # 각 방향에 벽 존재 여부 확인
 
 
 def move_penpen():  # 펜펜 움직이기
-    global pen_x, pen_y
+    global pen_x, pen_y, pen_d, pen_a
     if key == "Up":
-        if check_wall(pen_x, pen_y, DIR_UP) == False:
+        pen_d = DIR_UP
+        if check_wall(pen_x, pen_y, pen_d) == False:
             pen_y = pen_y - 60
     if key == "Down":
-        if check_wall(pen_x, pen_y, DIR_DOWN) == False:
+        pen_d = DIR_DOWN
+        if check_wall(pen_x, pen_y, pen_d) == False:
             pen_y = pen_y + 60
     if key == "Left":
-        if check_wall(pen_x, pen_y, DIR_LEFT) == False:
+        pen_d = DIR_LEFT
+        if check_wall(pen_x, pen_y, pen_d) == False:
             pen_x = pen_x - 60
     if key == "Right":
-        if check_wall(pen_x, pen_y, DIR_RIGHT) == False:
+        pen_d = DIR_RIGHT
+        if check_wall(pen_x, pen_y, pen_d) == False:
             pen_x = pen_x + 60
-
+            
+    pen_a = pen_d * 3 + ANIMATION[tmr % 4] #펜펜 에니메이션 번호 계산
 
 def main():  # 메인 루프
-    global key, koff
+    global key, koff, tmr
+    tmr = tmr + 1
     draw_screen()
     move_penpen()
     if koff == True:
@@ -101,7 +112,21 @@ img_bg = [
     tkinter.PhotoImage(file="image_penpen/chip02.png"),
     tkinter.PhotoImage(file="image_penpen/chip03.png")
 ]
-img_pen = tkinter.PhotoImage(file="image_penpen/pen03.png")
+#img_pen = tkinter.PhotoImage(file="image_penpen/pen03.png")
+img_pen = [
+    tkinter.PhotoImage(file="image_penpen/pen00.png"),
+    tkinter.PhotoImage(file="image_penpen/pen01.png"),
+    tkinter.PhotoImage(file="image_penpen/pen02.png"),
+    tkinter.PhotoImage(file="image_penpen/pen03.png"),
+    tkinter.PhotoImage(file="image_penpen/pen04.png"),
+    tkinter.PhotoImage(file="image_penpen/pen05.png"),
+    tkinter.PhotoImage(file="image_penpen/pen06.png"),
+    tkinter.PhotoImage(file="image_penpen/pen07.png"),
+    tkinter.PhotoImage(file="image_penpen/pen08.png"),
+    tkinter.PhotoImage(file="image_penpen/pen09.png"),
+    tkinter.PhotoImage(file="image_penpen/pen10.png"),
+    tkinter.PhotoImage(file="image_penpen/pen11.png")
+]
 
 root.title("아슬아슬 펭귄 미로")
 root.resizable(False, False)
